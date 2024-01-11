8a707b76dbf2436a7505b458132d9c0bbe1801e9
Muhammad Olamide <muhammadolammi@gmail.com>
2024-01-11 16:07:50 +0100

    try using custom port

--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
-	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/muhammadolammi/naijalocationserver/internal/database"
 )
@@ -18,21 +15,23 @@ type Config struct {
 }
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println(err)
-	}
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Println("there is no port provided kindly provide a port.")
-		return
-	}
-	port = "8080"
-	dbURL := os.Getenv("DB_URL")
-	if dbURL == "" {
-		log.Println("empty dbURL")
-		return
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	log.Println("there is no port provided kindly provide a port.")
+	// 	return
+	// }
+
+	// dbURL := os.Getenv("DB_URL")
+	// if dbURL == "" {
+	// 	log.Println("empty dbURL")
+	// 	return
+	// }
+	port := "8080"
+	dbURL := "postgresql://postgres:akek@localhost:5432/naijalocation?sslmode=disable"
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		fmt.Println(err)
